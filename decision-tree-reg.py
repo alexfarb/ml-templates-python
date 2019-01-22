46428c7e8dfc033bce78a308525f8c8e176c6d7d
Alexandre Farias <alexfarb@gmail.com>
2019-01-22 19:46:56 -0300

    Added Naive Bayes and Decision Trees (Regression and Classification).

---
+++ decision-tree-reg.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import cross_val_score
+import numpy as np
+# import graphviz
+
+# Read the dataset and store in a Pandas DataFrame
+df = pd.read_csv("C:\\repos\\dataset\\connekt\\regressao_1.csv")
+x = df.iloc[:, 0:-1] # Input Variables (Regressors)
+y = df.iloc[:, -1] # Target Variable
+x_train = x
+y_train = y
+x_test = x.iloc[0:1]
+y_test = y.iloc[0:1]
+# Fit the Model
+reg = tree.DecisionTreeRegressor().fit(x_train, y_train)
+# K-fold Cross Validation
+scores = cross_val_score(reg, x_train, y_train, cv=10)
+print("CV Scores: ", scores)
+# Mean from all CVs
+print("CV Scores Mean: ", np.mean(np.array(scores)))
+# Accuracy Mean
+# print("Accuracy Mean: ", clf.score(x_train, y_train))
+# Probability estimates
+# print("Probabiliy (Train):")
+# print(reg.predict_proba(x_train))
+# Independent term in the linear model
+# print("Independent Term: ", reg.intercept_)
+y_predicted = reg.predict(x_test)
+print("Predicted (Test): ", y_predicted)
+# Probability estimates
+# print("Probabiliy (Test): ", reg.predict_proba(x_test))
+# Plot the Tree
+# dot_data = tree.export_graphviz(clf, out_file=None)
+# graph = graphviz.Source(dot_data)
+# graph.render("Data")
+# dot_data = tree.export_graphviz(clf, out_file=None,
+#                      filled=True, rounded=True,
+#                      special_characters=True)
+# graph = graphviz.Source(dot_data)
+# graph
--- naive-bayes.py
+++ naive-bayes.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
 # Read the dataset and store in a Pandas DataFrame
-df = pd.read_csv("C:\\repos\\dataset\\connekt\\classificacao_1.csv")
+df = pd.read_csv("C:\\repos\\dataset\\connekt\\classificacao_2.csv")
 x = df.iloc[:, 0:-1] # Input Variables (Regressors)
 y = df.iloc[:, -1] # Target Variable
 x_train = x
@@ -13,14 +13,14 @@ y_train = y
 x_test = x.iloc[0:1]
 y_test = y.iloc[0:1]
 # Fit the Model
-clf = LogisticRegression(random_state=42, solver='lbfgs', multi_class='auto').fit(x_train, y_train)
+clf = GaussianNB().fit(x_train, y_train)
 # K-fold Cross Validation
 scores = cross_val_score(clf, x_train, y_train, cv=10)
 print("CV Scores: ", scores)
 # Mean from all CVs
 print("CV Scores Mean: ", np.mean(np.array(scores)))
 # Accuracy Mean
-print("Accuracy Mean: ", clf.score(x_train, y_train))
+# print("Accuracy Mean: ", clf.score(x_train, y_train))
 # Probability estimates
 print("Probabiliy (Train):")
 print(clf.predict_proba(x_train))
@@ -30,3 +30,5 @@ y_predicted = clf.predict(x_test)
 print("Predicted (Test): ", y_predicted)
 # Probability estimates
 print("Probabiliy (Test): ", clf.predict_proba(x_test))
+# clf_pf = GaussianNB()
+# print("Partial Fit: ", clf_pf.partial_fit(x_train, y_train, np.unique(y_train)))
